36544fdc6a6c97f9140f48ca63b06d6ba7b0cb57
Russidan-Nadee <russidan.nadee@gmail.com>
2025-04-23 11:58:12 +0700

    split_page

---
+++ rfid_project/lib/pages/search_and_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchAndDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search and Detail Page'),
+      ),
+      body: Center(
+        child: Text('This is the Search and Detail Page'),
+      ),
+    );
+  }
+}
